f41560000c61529fabbbcd4c9bdf771ce27c1c50
Kenan Gene Serran <Kenan.Serran@Focusservices.com>
2022-10-19 14:37:43 -0600

    added Child Pages

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -1,28 +1,40 @@
-
 <?php
 
 get_header();
 
-while(have_posts()) {
+while (have_posts()) {
     the_post(); ?>
-    
+
     <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title() ?></h1>
-        <div class="page-banner__intro">
-          <p>Learn how the school of your dreams got started.</p>
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title() ?></h1>
+            <div class="page-banner__intro">
+                <p>Replace later</p>
+            </div>
         </div>
-      </div>
     </div>
 
     <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-        </p>
-      </div>
+        <div class="metabox metabox--position-up metabox--with-home-link">
+            <?php
+            $theParent = wp_get_post_parent_id(get_the_ID());
 
+            if ($theParent) 
+
+                { ?>
+
+                    <p>
+                        <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent)?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent)?></a> <span class="metabox__main"><?php echo the_title() ?></span>
+                    </p>
+
+                <?php
+                }
+
+            ?>
+        </div>
+
+        <!--
       <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
@@ -30,10 +42,11 @@ while(have_posts()) {
           <li><a href="#">Our Goals</a></li>
         </ul>
       </div>
+    -->
 
-      <div class="generic-content">
-        <?php the_content() ?>
-      </div>
+        <div class="generic-content">
+            <?php the_content() ?>
+        </div>
     </div>
 
 <?php }
@@ -41,4 +54,3 @@ while(have_posts()) {
 get_footer();
 
 ?>
- 
