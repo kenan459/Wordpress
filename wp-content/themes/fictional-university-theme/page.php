f0f049eb5772c915bf33ae4d4bc7ebb2ef8170ad
Kenan Gene Serran <Kenan.Serran@Focusservices.com>
2022-10-21 11:26:13 -0600

    Dynamic Child Navigation added

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -19,38 +19,54 @@ while (have_posts()) {
         <div class="metabox metabox--position-up metabox--with-home-link">
             <?php
             $theParent = wp_get_post_parent_id(get_the_ID());
-            
-            echo get_the_ID();
-            echo "<br>";
-            echo $theParent;
 
-            if ($theParent) 
+            // echo get_the_ID();
+            // echo "<br>";
+            // echo $theParent;
 
-                { ?>
+            if ($theParent) { ?>
 
-                    <p>
-                        <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent)?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent)?></a> <span class="metabox__main"><?php echo the_title() ?></span>
-                    </p>
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?></a> <span class="metabox__main"><?php echo the_title() ?></span>
+                </p>
 
-                <?php
-                }
+            <?php
+            }
 
             ?>
+
         </div>
 
-    
-      <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
-        <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
-        </ul>
-      </div>
-   
+        <?php
+
+        $array = get_pages(array('child_of' => get_the_ID())); //This to check if the page has CHILD_PAGES or not
+
+        if ($theParent or $array) { ?>
+
+            <div class="page-links">
+                <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
+                <ul class="min-list">
+                    <?php
+                    if ($theParent) {
+                        $findChildrenOf = $theParent;
+                    } else {
+                        $findChildrenOf = get_the_ID();
+                    }
+                    wp_list_pages(array(
+                        'title_li' => NULL,
+                        'child_of' => $findChildrenOf,
+                        'sort_column' => 'menu_order'
+                    ));
+                    ?>
+                </ul>
+            </div>
+        <?php } ?>
+
 
         <div class="generic-content">
             <?php the_content() ?>
         </div>
+
     </div>
 
 <?php }
